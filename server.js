0da870cc59b4b5f063c9ba249962c78839bd8122
lopes-joe <slopes.joe@gmail.com>
2021-09-26 07:20:17 -0300

    criada a estrutura das rodas HTTP

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
-// EXPRESS - LEVANTAR SERVIDOR E CRIAR ROTAS HTTP
+// # EXPRESS - LEVANTAR SERVIDOR E CRIAR ROTAS HTTP
 
-// criar do "app" express 
+// ## criar do "app" express 
 const express = require('express')
 const app = express()
 
-// configurações do express
+// ## configurações do express
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-// levantar servidor
+// ## levantar servidor
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log("Servidor levantado na porta " + port)
 })
 
+// ## criar rotas
+app.route('/songs')
+    .get()
+    .post()
+app.route('/songs/:songId')
+    .get()
+    .put()
+    .delete()
 
 // BANCO DE DADOS - MONGO DB
